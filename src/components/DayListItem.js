077cefa138bb5f3bce47038d26f458a3a1a88a42
Brandon Felty <brcfelty@gmail.com>
2022-05-17 15:47:12 -0600

    adds output for spots remaining, tests added and passed

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -2,16 +2,27 @@ import React from "react";
 import classNames from "classnames";
 import 'components/DayListItem.scss'
 
+const formatSpots = (spots) => {
+  if (spots === 0) {
+    return 'no spots remaining';
+  }
+  if (spots === 1) {
+    return '1 spot remaining'
+  }
+  return `${spots} spots remaining`;
+}
+
 export default function DayListItem(props) {
   const dayClass = classNames({
     "day-list__item": true,
     "day-list__item--selected": props.selected,
     "day-list__item--full": !props.spots
   })
+  const spots = formatSpots(props.spots);
   return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2  className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{spots}</h3>
     </li>
   );
 }
