01e965b4f00107dfd721fe2b937804a1ac2a5042
Brandon Felty <brcfelty@gmail.com>
2022-05-18 17:34:14 -0600

    adds header, show, and empty to appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from "react";
+import React, { useState } from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
 
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Header from "components/Appointment/Header"
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 import "components/Appointment/styles.scss";
 
 const Appointment = (props) => {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview && <Show student={props.interview.student} interviewer={props.interview.interviewer} />}
+      {!props.interview && <Empty />}
+    </article>
   );
 }
 
