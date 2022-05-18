469393742bfa2052cc215794e4d9b602047d31b5
Brandon Felty <brcfelty@gmail.com>
2022-05-18 10:37:08 -0600

    adds empty component

--- stories/index.js
+++ stories/index.js
@@ -10,7 +10,8 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
-import Header from "components/Appointment/Header"
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 storiesOf("Button", module)
   .addParameters({
@@ -140,5 +141,6 @@ storiesOf("InterviewerList", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm"/>)
-  .add("Header", () => <Header time="12pm"/>);
+  .add("Header", () => <Header time="12pm"/>)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />);
 
