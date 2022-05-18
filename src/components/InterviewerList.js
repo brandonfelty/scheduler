494bd4d8d0838587b1f78f453d26aa39808e3f67
Brandon Felty <brcfelty@gmail.com>
2022-05-18 07:00:44 -0600

    interviewer list passes id to the item component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -8,6 +8,7 @@ export default function InterviewerList(props) {
     return (
       <InterviewerListItem
         key={interviewer.id}
+        id={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
         selected={interviewer.id === props.interviewer}
