51d7cae96e96300d1199e9bc309398fb4c8dea0a
Brandon Felty <brcfelty@gmail.com>
2022-07-21 14:29:00 -0600

    adds mock delete route

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -54,6 +54,14 @@ const fixtures = {
 };
 
 export default {
+  delete: jest.fn(url => {
+    if (url === "http://localhost:8001/api/appointments/2") {
+      return Promise.resolve({
+        status: 204,
+        statusText: "No Content"
+      })
+    };
+  }),
   put: jest.fn(url => {
     if (url === "http://localhost:8001/api/appointments/1") {
       return Promise.resolve({
